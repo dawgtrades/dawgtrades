376271c54e7a603c3ce0bc0eb5b356415fa1af27
chasesp <chase.ftw@gmail.com>
2014-11-18 12:50:53 -0500

    objmod

--- src/edu/uga/dawgtrades/model/impl/ObjectModelImpl.java
+++ src/edu/uga/dawgtrades/model/impl/ObjectModelImpl.java
@@ -21,7 +21,7 @@ public class ObjectModelImpl implements ObjectModel {
 	return attr;
     }
     @Override
-    public Attribute createAttribute(AttributeType attrType, Item item, String val) {
+    public Attribute createAttribute(AttributeType attrType, Item item, String val) throws DTException {
 	Attribute attr = new AttributeImpl(attrType, item, val);
 	return attr;
     }
@@ -32,7 +32,7 @@ public class ObjectModelImpl implements ObjectModel {
 	return attrt;
     }
     @Override
-    public AttributeType createAttributeType(Category cat, String name) {
+    public AttributeType createAttributeType(Category cat, String name) throws DTException {
 	AttributeType attrt = new AttributeType(cat, name);
 	return attrt;
     }
@@ -43,28 +43,131 @@ public class ObjectModelImpl implements ObjectModel {
 	return auc;
     }
     @Override
-    public Auction createAuction(Item item, float minPrice, Date expiration) {
+    public Auction createAuction(Item item, float minPrice, Date expiration) throws DTException {
 	Auction auc = new AuctionImpl(item, minPrice, minPrice, expiration, false);
 	return auc;
     }
+    @Override
     public Bid createBid() {
 	Bid bid = new BidImpl(null, null, null, null, null);
 	bid.setId(-1);
 	return bid;
     }
-    public Bid createBid(Auction auction, RegisteredUser user, float price) {
+    @Override
+    public Bid createBid(Auction auction, RegisteredUser user, float price) throws DTException {
 	Bid bid = new BidImpl(price, new Date(), true, auction, user);
 	return bid;
     }
+    @Override
     public Category createCategory() {
 	Category cat = new CategoryImpl(null, -1);
 	cat.setId(-1);
 	return cat;
     }
-    public Category createCategory(Category parent, String name) {
+    @Override
+    public Category createCategory(Category parent, String name) throws DTException {
 	Category cat = new CategoryImpl(name, parent);
 	return cat;
     }
+    @Override
     public ExperienceReport createExperienceReport() {
-	ExperienceReport erep = new ExperienceReportImpl(
+	ExperienceReport erep = new ExperienceReportImpl(null, null, null, -1, -1);
+	erep.setId(-1);
+	return erep;
+    }
+    @Override
+    public ExperienceReport createExperienceReport(RegisteredUser reviewer, RegisteredUser reviewed, int rating, String report, Date date) throws DTException {
+	ExperienceReport erep = new ExperienceReportImpl(rating, report, date, reviewer, reviewed);
+	return erep;
+    }
+    @Override
+    public Item createItem() {
+	Item item = new item(-1, -1, null, null, null);
+	item.setId(-1);
+	return item;
+    }
+    @Override
+    public Item createItem(Category category, RegisteredUser user, String identifier, String name, String description) throws DTException {
+	Item item = new item(user, category, identifier, name, description);
+	return item;
+    }
+    @Override
+    public Membership createMembership() {
+	Membership member = MembershipImpl(null, null);
+	member.setId(-1);
+	return member;
+    }
+    @Override
+    public Membership createMembership(float price, Date date) throws DTException {
+	Membership member = MembershipImpl(price, date);
+	return member;
+    }
+    @Override
+    public RegisteredUser createRegisteredUser() {
+	RegisteredUser user = RegisteredUserImpl(null, null, null, null, null, null, null, null);
+	user.setId(-1);
+	return user;
+    }
+    @Override
+    public RegisteredUser createRegisteredUser(String name, String firstName, String lastName, String password, boolean isAdmin, String email, String phone, boolean canText) throws DTException {
+	RegisteredUser user = RegisteredUserImpl(name, firstName, lastName, password, isAdmin, email, phone, canText);
+	return user;
+    }
+
+    @Override
+    public void deleteAttribute(Attribute attribute) throws DTException {
+	persistence.deleteAttribute(attribute);
+    }
+    @Override
+    public void deleteAttributeType(AttributeType attributeType) throws DTException {
+	persistence.deleteAttributeType(attributeType);
+    }
+    @Override
+    public void deleteAuction(Auction auction) throws DTException {
+	persistence.deleteAuction(auction);
+    }
+    @Override
+    public void deleteBid(Bid bid) throws DTException {
+	persistence.deleteBid(bid);
+    }
+    @Override
+    public void deleteCategory(Category category) throws DTException {
+	persistence.deleteCategory(category);
+    }
+    @Override
+    public void deleteExperienceReport(ExperienceReport experienceReport) throws DTException {
+	persistence.deleteExperienceReport(experienceReport);
+    }
+    @Override
+    public void deleteItem(Item item) throws DTException {
+	persistence.deleteItem(item);
+    }
+    @Override
+    public void deleteRegisteredUser(RegisteredUser registeredUser) throws DTException {
+	persistence.deleteRegisteredUser(registeredUser);
+    }
+
+    public Iterator<Auction> findAuction(Auction modelAuction) throws DTException {
+	return persistence.restoreAuction(modelAuction);
+    }
+    public Iterator<Bid> findBid(Bid modelBid) throws DTException {
+	return persistence.restoreBid(modelBid);
+    }
+    public Iterator<Category> findCategory(Category modelCategory) throws DTException {
+	return persistence.restoreCategory(modelCategory);
+    }
+    public Iterator<ExperienceReport> findExperienceReport(ExperienceReport modelExperienceReport) throws DTException {
+	return persistence.restoreExperienceReport(modelExperienceReport);
+    }
+    public Iterator<Item> findItem(Item modelItem) {
+	return persistence.restoreItem(modelItem);
+    }
+    public Membership findMembership() {//needs to be different?
+	return persistence.restoreMembership();
+    }
+    public Iterator<RegisteredUser> findRegisteredUser(RegisteredUser modelRegisteredUser) throws DTException {
+	return persistence.restoreRegisteredUser(modelRegisteredUser);
+    }
+    public Iterator<Attribute> getAttribute(Item item) throws DTException {
+
 }

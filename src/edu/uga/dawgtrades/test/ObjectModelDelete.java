f91f45aaa3180ec02752e9a2dab4e35e08a6a2c8
Vic Lawson <vlawson3@gmail.com>
2014-11-18 23:39:01 -0500

    vics objectModelDelete 1 case

--- src/edu/uga/dawgtrades/test/ObjectModelDelete.java
+++ src/edu/uga/dawgtrades/test/ObjectModelDelete.java
@@ -1,21 +1,25 @@
-package edu.uga.clubs.test;
+package edu.uga.dawgtrades.test;
 
 import java.sql.Connection;
-import java.util.Iterator;
+import java.util.Date;
 
-import edu.uga.clubs.ClubsException;
-import edu.uga.clubs.model.Club;
-import edu.uga.clubs.model.Membership;
-import edu.uga.clubs.model.ObjectModel;
-import edu.uga.clubs.model.Person;
-import edu.uga.clubs.model.impl.ObjectModelImpl;
-import edu.uga.clubs.persistence.Persistence;
-import edu.uga.clubs.persistence.impl.DbUtils;
-import edu.uga.clubs.persistence.impl.PersistenceImpl;
+import edu.uga.dawgtrades.model.Attribute;
+import edu.uga.dawgtrades.model.AttributeType;
+import edu.uga.dawgtrades.model.Auction;
+import edu.uga.dawgtrades.model.Bid;
+import edu.uga.dawgtrades.model.Category;
+import edu.uga.dawgtrades.model.ExperienceReport;
+import edu.uga.dawgtrades.model.Item;
+import edu.uga.dawgtrades.model.Membership;
+import edu.uga.dawgtrades.model.ObjectModel;
+import edu.uga.dawgtrades.model.RegisteredUser;
+import edu.uga.dawgtrades.model.impl.ObjectModelImpl;
+import edu.uga.dawgtrades.persist.Persistence;
+import edu.uga.dawgtrades.persist.impl.DbUtils;
+import edu.uga.dawgtrades.persist.impl.PersistenceImpl;
 
-
-// A class to illustrate how to delete entity objects and associations
-//
+// Testing the deletion of the entity classes
+// and associations for DawgTrades
 public class ObjectModelDelete
 {
     public static void main(String[] args)
@@ -41,101 +45,31 @@ public class ObjectModelDelete
          // connect the ObjectModel module to the Persistence module
          objectModel.setPersistence( persistence ); 
          
-         Iterator<Club> clubIter = null;
+         Iterator<RegisteredUser> userIter = null;
                   
          try {
              
-	     // Delete the Running club object
-             // First: find the Running club
-             Club runningClub = null;
-             Club modelClub = objectModel.createClub();
-             modelClub.setName( "Running" );
-             clubIter = objectModel.findClub( modelClub );
-             while( clubIter.hasNext() ) {
-                 runningClub = clubIter.next();
-                 System.out.println( runningClub );
+	         // Delete the RegisteredUser object
+             // First: find the RegisteredUser club
+             RegisteredUser registeredUser = null;
+             RegisteredUser modelUser = objectModel.createRegisteredUser();
+             modelUser.setName( "FirstUser" );
+             userIter = objectModel.findRegisteredUser( modelUser );
+             while( userIter.hasNext() ) {
+                 registeredUser = userIter.next();
+                 System.out.println( registeredUser );
              }
-             
-             // Second: delete the Running club
-             if( runningClub != null ) {
-                 objectModel.deleteClub( runningClub );
-                 System.out.println( "Deleted the Running club" );
-             }
-             else
-                 System.out.println( "Failed to retrieve the Running Club object" );
-             
-	     // Delete Heather Brooks Person object
-             // First: find Heather Brooks
-             Person heatherBrooks = null;
-             Person modelPerson = objectModel.createPerson();
-             modelPerson.setFirstName( "Heather" );
-             modelPerson.setLastName( "Brooks" );
-             Iterator<Person> personIter = objectModel.findPerson( modelPerson );
-             while( personIter.hasNext() ) {
-                 heatherBrooks = personIter.next();
-                 System.out.println( heatherBrooks );
-             }
-             
-             // Second: delete Heather Brooks
-             if( heatherBrooks != null ) {
-                 objectModel.deletePerson( heatherBrooks );
-                 System.out.println( "Deleted the Heather Brooks Person object" );
+             // Second: delete the Registered User
+             if( registeredUser != null ) {
+                 objectModel.deleteClub( registeredUser );
+                 System.out.println( "Deleted the registered User" );
              }
              else
-                 System.out.println( "Failed to find Heather Brooks" );
-             
-             // Delete the membership of Robert Wilson in the Bridge club
-             // First: locate the Bridge club
-             Club bridgeClub = null;
-             modelClub = objectModel.createClub();
-             modelClub.setName( "Bridge" );
-             clubIter = objectModel.findClub( modelClub );
-             while( clubIter.hasNext() ) {
-                 bridgeClub = clubIter.next();
-                 System.out.println( bridgeClub );
-             }
-             
-             if( bridgeClub == null )
-                 System.out.println( "Failed to retrieve the Bridge club object" );
-             
-             // Second: locate Robert Wilson
-             Person robertWilson = null;
-             modelPerson = objectModel.createPerson();
-             modelPerson.setFirstName( "Robert" );
-             modelPerson.setLastName( "Wilson" );
-             personIter = objectModel.findPerson( modelPerson );
-             while( personIter.hasNext() ) {
-                 robertWilson = personIter.next();
-                 System.out.println( robertWilson );
-             }
-             
-             if( robertWilson == null )
-                 System.out.println( "Failed to retrieve Robert Wilson person object" );
-             
-             // Third: locate the Membership record of Robert Wilson in the Bridge club
-             if( bridgeClub != null && robertWilson != null ) {
-                 Membership robertInbridgeMembership = null;
-                 Membership modelMembership = objectModel.createMembership();
-                 modelMembership.setClub( bridgeClub );
-                 modelMembership.setPerson( robertWilson );
-                 Iterator<Membership> membershipIter = objectModel.findMembership( modelMembership );
-                 while( membershipIter.hasNext() ) {
-                     robertInbridgeMembership = membershipIter.next();
-                     System.out.println( robertInbridgeMembership );
-                 }
-
-                 // delete the Membership record of Robert Wilson in the Bridge club
-                 if( robertInbridgeMembership != null ) {
-                     objectModel.deleteMembership( robertInbridgeMembership );
-                     System.out.println( "Deleted the membership of Robert Wilson in the Bridge club" );
-                 }
-                 else
-                     System.out.println( "Failed to retrieve the membership of Robert Wilson in the Bridge club" );
-             }
-
-         }
-         catch( ClubsException ce ) {
-             System.err.println( "ClubsException: " + ce );
+                 System.out.println( "Failed to retrieve the registered User object" );
+				 
+          }   
+         catch( DTException ce ) {
+             System.err.println( "DTException: " + ce );
          }
          catch( Exception e ) {
              System.err.println( "Exception: " + e );

f28e918692cabd5dd74b4a3820d6ad2faf65cb3d
chasesp <chase.ftw@gmail.com>
2014-11-19 17:14:02 -0500

    AttributeManager

--- src/edu/uga/dawgtrades/persist/impl/AttributeManager.java
+++ src/edu/uga/dawgtrades/persist/impl/AttributeManager.java
@@ -112,4 +112,133 @@ public class AttributeManager {
 	    if(stmt.execute(query.toString())) {
 		ResultSet r = stmt.getResultSet();
 		return new ItemIterator(r, objectModel);
+	    }
+	}
+	catch(Exception e) {
+	    throw new DTException("AttributeManager.restore: Could not restore persistent attribute obj because " + e);
+	}
+	throw new DTException("AttributeManager.restore: Could not restore persistent attribute obj");
+    }
+
+    public AttributeType restoreAttributeType(Attribute attr) throws DTException {
+	String selectAttributeSql = "select at.attribute_type_id, at.attribute_type_name, at.category_id from attribute a, attribute_type at where a.attribute_type_id = at.attribute_type_id";
+	Statement stmt = null;
+	StringBuffer query = new StringBuffer(100);
+	StringBuffer condition = new StringBuffer(100);
+
+	condition.setLength(0);
+
+	query.append(selectAttributeSql);
+
+	if(attr != null) {
+	    if(attr.getId() >= 0)
+		query.append(" and a.attribute_id = '" + attr.getId() + "'");
+	    else {
+		if(attr.getValue() != null)
+		    condition.append(" a.attribute_value = '" + attr.getValue() + "'");
+		if(attr.getAttributeType() >= 0) {
+		    if(condition.length() == 0)
+		       condition.append(" and");
+		       condition.append(" a.attribute_type_id = '" + attr.getAttributeType() + "'");
+		}
+		if(attr.getItemId() >= 0) {
+		    if(condition.length() == 0)
+			condition.append(" and");
+		    condition.append(" a.item_id = '" + attr.getItemId() + "'");
+		}
+		if(condition.length() > 0)
+		    query.append(condition);
+	    }
+	}
+
+	try {
+	    stmt = conn.createStatement();
+
+	    if(stmt.execute(query.toString())) {
+		ResultSet r = stmt.getResultSet();
+		Iterator<AttributeType> attrTIter = new AttributeTypeIterator(r, objectModel);
+		if(attrTIter != null && attrTIter.hasNext())
+		    return attrTIter.next();
+		else
+		    return null;
+	    }
+	}
+	catch(Exception e) {
+	    throw new DTException("AttributeManager.restoreAttributeType: Could not restore persistent AttributeType objs because " + e);
+	}
+	throw new DTException("AttributeManager.restoreAttributeType: Could not restore persistent AttributeType objs");
+    }
+
+    public Item restoreItem(Attribute attr) throws DTException {
+	String selectAttributeSql = "select i.item_id, i.name, i.category_id, i.identifier, i.description, i.owner_id from attribute a, item i where a.item_id = i.item_id";
+	Statement stmt = null;
+	StringBuffer query = new StringBuffer(100);
+	StringBuffer condition = new StringBuffer(100);
+
+	condition.setLength(0);
+
+	query.append(selectAttributeSql);
+
+	if(attr != null) {
+	    if(attr.getId() >= 0)
+		query.append(" and a.attribute_id = '" + attr.getId() + "'");
+	    else {
+		if(attr.getValue() != null)
+		    condition.append(" a.attribute_value = '" + attr.getValue() + "'");
+		if(attr.getAttributeType() >= 0) {
+		    if(condition.length() == 0)
+			condition.append(" and");
+		    condition.append(" a.attribute_type_id = '" + attr.getAttributeType() + "'");
+		}
+		if(attr.getItemId() >= 0) {
+		    if(condition.length() == 0)
+			condition.append(" and");
+		    condition.append(" a.item_id = '" + attr.getItemId() + "'");
+		}
+		if(condition.length() > 0)
+		    query.append(condition);
+	    }
+	}
+	try {
+	    stmt = conn.createStatement();
+
+	    if(stmt.execute(query.toString())) {
+		ResultSet r = stmt.getResultSet();
+		Iterator<Item> itemIter = new ItemIterator(r, objectModel);
+		if(itemIter != null && itemIter.hasNext())
+		    return itemIter.next();
+		else
+		    return null;
+	    }
+	}
+	catch(Exception e) {
+	    throw new DTException("AttributeManager.restoreItem: Could not restore persistent Item objs because " + e);
+	}
+	throw new DTException("AttributeManager.restoreItem: Could not restore persistent Item objs");
+    }
+
+    public void delete(Attribute attr) throws DTException {
+	String deleteAttributeSql = "delete from attribute where attribute_id = ?";
+	PreparedStatement stmt = null;
+	int numUpdated;
+
+	if(!attr.isPersistent())
+	    return;
+
+	try {
+	    stmt = (PreparedStatement) conn.prepareStatement(deleteAttributeSql);
+
+	    stmt.setLong(1, attribute.getId());
+
+	    numUpdated = stmt.executeUpdate();
+
+	    if(numUpdated == 0) {
+		throw new DTException("AttributeManager.delete: failed to delete Attribute");
+	    }
+	}
+	catch(SQLException e) {
+	    throw new DTException("AttributeManager.delete: failed to delete Attribute: " + e.getMessage());
+	}
+    }
+
 }
